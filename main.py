b2fe88a93bf5d4f120f84dab87bb365359a07df7
Karsten Meichser <karsten.meichsner@iav.de>
2023-12-12 16:43:27 +0100

    update main.py

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from aoc_2023 import helpers, day01, day02, day03, day04, day05, day06, day07, d
 
 content = helpers.ContentLoader()
 
-day = 10
-
-#if datetime.datetime.now().date() > datetime.date(2023, 12, 25):
-#    day = input("choose a day (1 ... 25): ")
-#else:
-#    day = datetime.datetime.now().day
+if datetime.datetime.now().date() > datetime.date(2023, 12, 25):
+    day = input("choose a day (1 ... 25): ")
+else:
+    day = datetime.datetime.now().day
 
 if day == 1:
     content.set_filename("./aoc_2023/input_01.txt")
