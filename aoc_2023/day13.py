50a2c672c9e59a03e46110e4c671039ffee93e17
Karsten Meichser <karsten.meichsner@iav.de>
2023-12-15 21:33:43 +0100

    day 13

--- aoc_2023/day13.py
+++ aoc_2023/day13.py
@@ -16,44 +16,41 @@ class PointOfIncidence():
             y += 1
         self.areas.append(this_area)
 
-    def rotate(self, field):
-        new_field = []
+    def rotate(self, area):
+        new_area = []
         i = 0
-        while i < len(field[0]):
+        while i < len(area[0]):
             new_line = ""
-            for line in field:
+            for line in area:
                 new_line += line[i]
             i += 1
-            new_field.append(new_line)
-        return new_field
+            new_area.append(new_line)
+        return new_area
             
     def summared_notes(self):
         s = 0
-
         for area in self.areas:                
             j = 0
             while j < len(area) -1 :
-                if area[j] == area[j + 1]:
-                    m = 1
-                    is_mirror = True
-                    while (j - m >= 0) and (j + m + 1 < len(area)):
-                        if area[j - m] != area[j + m + 1]:
-                            is_mirror = False
-                        m += 1
-                    if is_mirror:
-                        s += (j + 1) * 100
+                if self.is_mirror(area=area, j=j):
+                    s += (j + 1) * 100
                 j += 1
             j = 0
             area = self.rotate(area)
             while j < len(area) -1 :
-                if area[j] == area[j + 1]:
-                    m = 1
-                    is_mirror = True
-                    while (j - m >= 0) and (j + m + 1 < len(area)):
-                        if area[j - m] != area[j + m + 1]:
-                            is_mirror = False
-                        m += 1
-                    if is_mirror:
-                        s += (j + 1)
+                if self.is_mirror(area=area, j=j):
+                    s += (j + 1)
                 j += 1
         return s
+    
+    def is_mirror(self, area, j):
+        if area[j] == area[j + 1]:
+            m = 1
+            is_mirror = True
+            while (j - m >= 0) and (j + m + 1 < len(area)):
+                if area[j - m] != area[j + m + 1]:
+                    is_mirror = False
+                m += 1
+            if is_mirror:
+                return True
+        return False
